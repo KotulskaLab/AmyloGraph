f51d7fb7292adf67cb562d68fdc3aa8cd8802dd1
DominikRafacz <d.rafacz@student.mini.pw.edu.pl>
2021-07-01 11:48:29 +0200

    filter out unnecessary columns

--- R/edgeTable.R
+++ R/edgeTable.R
@@ -8,7 +8,12 @@ edgeTableUI <- function(id) {
 edgeTableServer <- function(id, edges) {
   moduleServer(id, function(input, output, session) {
     output[["table"]] <- renderDataTable(
-      edges,
+      edges %>% select(interactor_name,
+                       interactee_name,
+                       aggregation_speed,
+                       elongates_by_attaching,
+                       heterogenous_fibers,
+                       doi),
       options = list(
         scrollY = "calc(100vh - 330px - var(--correction))",
         scrollCollapse = TRUE
