76558d76dccde79f1cfda2b0f682e470fa13ed27
DominikRafacz <d.rafacz@student.mini.pw.edu.pl>
2021-07-08 11:41:23 +0200

    remove greek letters

--- transform_data.R
+++ transform_data.R
@@ -1,6 +1,14 @@
 library(dplyr)
+library(stringi)
 
 AG_data <- readRDS("AmyloGraph.RDS")
+
+greek_letters <- c("α", "β", "κ")
+greek_letter_names <- c("alpha", "beta", "kappa")
+degreekize <- function(names) {
+  stri_replace_all_fixed(names, greek_letters, greek_letter_names, vectorize_all = FALSE) %>%
+    stri_trans_totitle(type = 'sentence')
+}
 
 AG_data %>%
   select(interactor_name = `Interactor name`,
@@ -9,4 +17,7 @@ AG_data %>%
          elongates_by_attaching = `If interactee is still forming fibrils after the interaction, do fibrils of interactee elongates by attaching to monomers/oligomers/fibrils of interactor?`,
          heterogenous_fibers = `Is interaction resulting in heterogeneous fibrils consisting of interactor and interactee molecules?`,
          doi = `DOI (only number, no the link, e.g., 10.1073/pnas.1610371113)`) %>%
+  mutate(interactor_name = degreekize(interactor_name),
+         interactee_name = degreekize(interactee_name)) %>%
+  
   write.csv("interactions_data.csv", row.names = FALSE)
