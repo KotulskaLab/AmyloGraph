c431e6c0f512c0afe95002f58598b636106d0de9
Laura <erdaradun.gaztea@gmail.com>
2021-07-13 12:12:31 +0200

    added namespace parameter to ifelsePanel

--- R/ifelsePanel.R
+++ R/ifelsePanel.R
@@ -1,14 +1,17 @@
-ifelsePanel <- function(id, condition, content_true, content_false) {
+ifelsePanel <- function(id, condition, content_true, content_false,
+                        ns = NS(NULL)) {
   tagList(
     conditionalPanel(
       id = NS(id, "pos"),
       condition = condition,
-      content_true
+      content_true,
+      ns = ns
     ),
     conditionalPanel(
       id = NS(id, "neg"),
       condition = glue("!({condition})"),
-      content_false
+      content_false,
+      ns = ns
     )
   )
 }
