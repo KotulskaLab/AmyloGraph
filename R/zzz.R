483a73f0e1f80bf67a4413df7fc594cda81fe629
Laura <erdaradun.gaztea@gmail.com>
2021-07-14 14:24:53 +0200

    hardcoded palette values

--- R/zzz.R
+++ R/zzz.R
@@ -3,7 +3,8 @@
   
   new_options <- list(
     ag_str_null = "null", 
-    ag_palette = palette("Dark 2")
+    ag_palette = c("#1B9E77", "#D95F02", "#7570B3", "#E7298A",
+                   "#66A61E", "#E6AB02", "#A6761D", "gray40")
   )
   
   unset_inds <- !(names(new_options) %in% names(prev_options))
