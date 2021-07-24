9697aee85b07f5745359df428848d844c2fdbfea
Laura <erdaradun.gaztea@gmail.com>
2021-07-24 12:48:36 +0200

    isolate read-only input

--- R/vis.R
+++ R/vis.R
@@ -4,7 +4,7 @@ visResetEdges <- \(graph, edges, input, selected_node_input_id,
                    legend = FALSE)
   graph %>%
     visGetEdges("graph_edges_") %>%
-    visRemoveEdges(seq_along(input[["graph_edges_"]])) %>%
+    visRemoveEdges(seq_along(isolate(input[["graph_edges_"]]))) %>%
     visUpdateEdges(edges, legend) %>%
     visSelectNodes(input[[selected_node_input_id]])
 
