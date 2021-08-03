22f291b4eb7ecd26c700cb2ed0546e8d994c7adc
DominikRafacz <d.rafacz@student.mini.pw.edu.pl>
2021-08-03 12:07:35 +0200

    truncate long dois
    closes #12 definitely

--- R/linfiky_doi.R
+++ R/linfiky_doi.R
@@ -1,4 +1,5 @@
 #' @importFrom glue glue
+#' @importFrom stringr str_trunc
 linkify_doi <- function(doi) {
-  glue("<a href='https://doi.org/{doi}' target='_blank' rel='noopener noreferer'>{doi}</a>")
+  glue("<a href='https://doi.org/{doi}' target='_blank' rel='noopener noreferer'>{str_trunc(doi, 18)}</a>")
 }
