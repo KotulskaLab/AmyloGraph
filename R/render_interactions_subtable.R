09ab2828fe41d018b89ccbec65bf7a67ede1efaa
DominikRafacz <d.rafacz@student.mini.pw.edu.pl>
2021-08-18 15:38:17 +0200

    add details selection buttons for interactions subtable

--- R/render_interactions_subtable.R
+++ R/render_interactions_subtable.R
@@ -5,8 +5,9 @@ render_interactions_subtable <- function(input, edge_data, target_id, target_var
     edge_data[["node_info"]] %>%
       filter({{target_id}} == input[["select_node"]]) %>%
       arrange({{target_variable}}, doi) %>%
-      mutate(doi = linkify_doi(doi)) %>%
-      select(AGID, {{target_variable}}, doi)
+      mutate(doi = linkify_doi(doi),
+             details = details_button(AGID)) %>%
+      select(AGID, details, {{target_variable}}, doi)
   }, options = list(
     pageLength = 10,
     lengthChange = FALSE
