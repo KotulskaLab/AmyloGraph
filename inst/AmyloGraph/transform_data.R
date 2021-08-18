3dea7583dce647f62b57aa0e10439dba3c7701a0
DominikRafacz <d.rafacz@student.mini.pw.edu.pl>
2021-08-18 16:02:52 +0200

    heuristic fix of sequences

--- inst/AmyloGraph/transform_data.R
+++ inst/AmyloGraph/transform_data.R
@@ -9,6 +9,13 @@ greek_letters <- c("^α", "^β", "^δ", "^κ", "α", "β", "κ", "δ")
 greek_letter_names <- c("Alpha", "Beta", "Delta", "Kappa", "alpha", "beta", "delta", "kappa")
 degreekize <- function(names) stri_replace_all_regex(names, greek_letters, greek_letter_names, vectorize_all = FALSE)
 
+sequentize <- function(sequences) 
+  stri_replace_all_regex(sequences, "\n", "", ) %>%
+  stri_replace_all_regex("[0-9]", "") %>%
+  stri_trans_toupper %>%
+  stri_extract_last_regex("[A-Z]+$")
+  
+
 AG_data %>%
   select(interactor_name = `Interactor name`,
          interactee_name = `Interactee name`,
@@ -20,5 +27,7 @@ AG_data %>%
          doi = `DOI (only number, no the link, e.g., 10.1073/pnas.1610371113)`) %>%
   mutate(interactor_name = degreekize(interactor_name),
          interactee_name = degreekize(interactee_name),
+         interactor_sequence = sequentize(interactor_sequence),
+         interactee_sequence = sequentize(interactee_sequence),
          AGID = glue("AG{str_pad(cur_group_rows(), 5, 'left', '0')}")) %>%
   write.csv("inst/AmyloGraph/interactions_data.csv", row.names = FALSE)
