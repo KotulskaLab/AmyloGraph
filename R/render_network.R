2e9c92dfaf645412666f9ce5a5eec09383b22fb4
DominikRafacz <d.rafacz@student.mini.pw.edu.pl>
2021-09-02 13:35:50 +0200

    change default colors of nodes and edges

--- R/render_network.R
+++ R/render_network.R
@@ -1,12 +1,16 @@
 #' @importFrom visNetwork renderVisNetwork visNetwork visEdges visLayout visOptions visInteraction
-#' @importFrom visNetwork visEvents visIgraphLayout visExport
+#' @importFrom visNetwork visEvents visIgraphLayout visExport visNodes
 render_network <- function(ag_data_nodes, edges) {
   renderVisNetwork({
     # we don't want to render graph each time we modify edges
     # instead we remove and update them in a separate observer
     visNetwork(ag_data_nodes, isolate(edges[["graph"]]),
                width = 1600, height = 900) %>%
-      visEdges(arrows = "to", width = 2)  %>% 
+      visEdges(arrows = "to", 
+               width = 2,
+               color = "#3674AB") %>% 
+      visNodes(color = "#F3C677",
+               font = list(color = "#0C0A3E")) %>%  
       visLayout(randomSeed = 1337) %>% 
       visOptions(
         highlightNearest = list(enabled = TRUE, degree = 1,
