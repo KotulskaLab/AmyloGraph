f4df17ccec3dff38ac5a5b86368ae26efd61ad3f
Laura <32573962+ErdaradunGaztea@users.noreply.github.com>
2021-09-07 19:02:57 +0200

    move linkify_doi to utils

--- R/linfiky_doi.R
+++
@@ -1,5 +0,0 @@
-#' @importFrom glue glue
-#' @importFrom stringr str_trunc
-linkify_doi <- function(doi) {
-  glue("<a href='https://doi.org/{doi}' target='_blank' rel='noopener noreferer'>{str_trunc(doi, 18)}</a>")
-}
--- R/util.R
+++ R/util.R
@@ -6,3 +6,9 @@ ag_option <- function(option) {
 is_node_selected <- function(id) {
   length(id) != 0 && id != ag_option("str_null")
 }
+
+#' @importFrom glue glue
+#' @importFrom stringr str_trunc
+linkify_doi <- function(doi) {
+  glue("<a href='https://doi.org/{doi}' target='_blank' rel='noopener noreferer'>{str_trunc(doi, 18)}</a>")
+}
