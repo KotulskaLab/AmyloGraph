86be152d880599ccf78df72cfa899172b5a4094e
Laura <32573962+ErdaradunGaztea@users.noreply.github.com>
2021-09-08 18:08:08 +0200

    add horizontal scroll to main table
    close #24

--- R/render_interactions_table.R
+++ R/render_interactions_table.R
@@ -3,6 +3,7 @@ render_interactions_table <- function(interactions_table) renderDataTable(
   interactions_table(),
   options = list(
     scrollY = "calc(100vh - 330px - var(--correction))",
+    scrollX = TRUE,
     scrollCollapse = TRUE
   ),
   escape = FALSE,
