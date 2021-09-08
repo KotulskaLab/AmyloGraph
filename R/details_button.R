67db9275f5b0502ee8e4552a2cf1af4e627677fd
Laura <32573962+ErdaradunGaztea@users.noreply.github.com>
2021-09-08 20:24:28 +0200

    Revert "fix id generation for details buttons"
    This reverts commit 1b4e2798b14e0379230896d30a4bb6023392317c.

--- R/details_button.R
+++ R/details_button.R
@@ -5,7 +5,7 @@ details_button <- \(interaction_ids, id, ns, ...) {
   map_chr(interaction_ids, 
           \(interaction_id) as.character(
             #should this values of id be generated manually with NS like this?
-            actionButton(glue("{ns(paste0('interaction_view_selector_', interaction_id))}"), 
+            actionButton(glue("ns(interaction_view_selector_{interaction_id})"), 
                          "Details",
                          onclick = glue("Shiny.setInputValue('{NS('single_interaction', 'selected_interaction')}', '{interaction_id}')"))))
 }
