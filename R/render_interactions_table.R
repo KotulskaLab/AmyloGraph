a8b1aae1d681ce844bfed18fd0e358c71ae8eaa4
Laura <erdaradun.gaztea@gmail.com>
2021-09-17 11:49:44 +0200

    add download buttons

--- R/render_interactions_table.R
+++ R/render_interactions_table.R
@@ -3,13 +3,30 @@ render_interactions_table <- function(interactions_table)
   renderDataTable(
     interactions_table(),
     options = list(
+      dom = "lBfrtip",
       scrollY = "calc(100vh - 330px - var(--correction))",
       scrollX = TRUE,
       scrollCollapse = TRUE,
-      lengthMenu = list(c(10, 25, 50, 100, -1), c("10", "25", "50", "100", "All"))
+      lengthMenu = list(c(10, 25, 50, 100, -1), c("10", "25", "50", "100", "All")),
+      buttons = list("colvis", list(
+        extend = "collection",
+        text = "Download",
+        buttons = list(
+          list(
+            extend = "csv",
+            filename = "AmyloGraph"
+          ),
+          list(
+            extend = "excel",
+            filename = "AmyloGraph"
+          )
+        )
+      ))
     ),
     escape = FALSE,
     filter = "top",
     rownames = FALSE,
-    colnames = ag_colnames(interactions_table())
+    colnames = ag_colnames(interactions_table()),
+    extensions = "Buttons",
+    server = FALSE
   )
