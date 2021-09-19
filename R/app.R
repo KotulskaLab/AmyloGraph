15c99f6331d5dadfdc25681d4490f1f8ef6727a6
Michał Burdukiewicz <michalburdukiewicz@gmail.com>
2021-09-19 21:40:18 +0200

    added few imports from shiny

--- R/app.R
+++ R/app.R
@@ -1,4 +1,4 @@
-#' @importFrom shiny runApp
+#' @importFrom shiny checkboxInput h2 h4 includeMarkdown mainPanel renderText runApp textInput textOutput verbatimTextOutput 
 #' @export
 AmyloGraph <- function()
   runApp(system.file("AmyloGraph", package = "AmyloGraph"))
