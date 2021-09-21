049614834c199d21c948749d86586b910496cb98
Laura <erdaradun.gaztea@gmail.com>
2021-09-21 12:42:10 +0200

    modify download buttons to use downloadHandler

--- R/module_interactions_table.R
+++ R/module_interactions_table.R
@@ -1,16 +1,31 @@
 #' @importFrom shiny NS
 ui_interactions_table <- function(id) {
   ns <- NS(id)
-  elem_interactions_table(ns("table"))
+  tagList(
+    downloadButton(ns("download_csv"), "", "ag_hidden_btn"),
+    downloadButton(ns("download_xlsx"), "", "ag_hidden_btn"),
+    elem_interactions_table(ns("table"))
+  )
 }
 
 #' @importFrom shiny moduleServer NS
+#' @importFrom shinyjs hideElement
+#' @importFrom openxlsx write.xlsx
 server_interactions_table <- function(id, edges) {
   moduleServer(id, function(input, output, session) {
     ns <- NS(id)
     
-    interactions_table <- reactive_interactions_table(edges, ns) 
+    interactions_table <- reactive_interactions_table(edges, ns)
     
-    output[["table"]] <- render_interactions_table(interactions_table)  
+    output[["table"]] <- render_interactions_table(interactions_table, ns)
+    
+    output[["download_csv"]] <- downloadHandler(
+      filename = \() "AmyloGraph.csv",
+      content = \(file) write.csv(iris, file)
+    )
+    output[["download_xlsx"]] <- downloadHandler(
+      filename = \() "Amylograph.xlsx",
+      content = \(file) write.xlsx(iris, file)
+    )
   })
 }
