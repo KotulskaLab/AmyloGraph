18f55161759b07febbc5848f901197b05e00a105
Laura <erdaradun.gaztea@gmail.com>
2021-09-22 19:19:29 +0200

    display Interaction tab only after choosing an interaction

--- R/observe_interaction_selection.R
+++ R/observe_interaction_selection.R
@@ -1,7 +1,16 @@
-#' @importFrom shiny observe req updateTabsetPanel
-observe_interaction_selection <- \(input) observe({
-  req(input[[NS("single_interaction", "selected_interaction")]])
-  updateTabsetPanel(inputId = "app_main_panel",
-                    selected = "single_interaction")
+#' @importFrom shiny observe req updateTabsetPanel hideTab showTab
+observe_interaction_selection <- \(input) {
+  observe({
+    if (is.null(input[[NS("single_interaction", "selected_interaction")]]))
+      hideTab(inputId = "app_main_panel", target = "single_interaction")
+    else
+      showTab(inputId = "app_main_panel", target = "single_interaction")
+  })
   
-})
+  observe({
+    req(input[[NS("single_interaction", "selected_interaction")]])
+    updateTabsetPanel(inputId = "app_main_panel",
+                      selected = "single_interaction")
+    
+  })
+}
