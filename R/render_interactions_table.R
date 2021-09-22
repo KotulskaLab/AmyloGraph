7bd588abd3c9d31a0e615ebcbde9e71e9bcb90a9
Laura <erdaradun.gaztea@gmail.com>
2021-09-22 23:08:03 +0200

    add select functionality to main table

--- R/render_interactions_table.R
+++ R/render_interactions_table.R
@@ -30,13 +30,16 @@ render_interactions_table <- function(interactions_table, ns, session)
       scrollX = TRUE,
       scrollCollapse = TRUE,
       lengthMenu = list(c(10, 25, 50, 100, -1), c("10", "25", "50", "100", "All")),
-      buttons = "colvis"
+      buttons = c("selectAll", "selectNone", "colvis"),
+      select = list(style = "multi+shift", items = "row")
     ),
     escape = FALSE,
     filter = "top",
     rownames = FALSE,
     colnames = ag_colnames(interactions_table()),
-    extensions = "Buttons",
+    extensions = c("Select", "Buttons"),
+    selection = "none",
+    server = FALSE,
     callback = JS(
       download_button_callback(ns, session, "download_csv", "Download as CSV"),
       download_button_callback(ns, session, "download_xlsx", "Download as Excel"),
