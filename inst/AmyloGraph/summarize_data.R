80cf15c775b6c3844ac278f7382a18864854253e
Michał Burdukiewicz <michalburdukiewicz@gmail.com>
2021-09-25 18:01:33 +0200

    added database summary plot

--- inst/AmyloGraph/summarize_data.R
+++ inst/AmyloGraph/summarize_data.R
@@ -1,12 +1,14 @@
 library(ggplot2)
 
+about_section <- TRUE
+
 dat <- readr::read_csv(system.file("AmyloGraph", "interactions_data.csv", package = "AmyloGraph"),
-         col_types = "ccccfffcc")
+                       col_types = "ccccfffcc")
 
 nrow(dat)
 length(unique(dat[["doi"]]))
 
-table(dat[["doi"]]) %>% 
+p <- table(dat[["doi"]]) %>% 
   data.frame() %>% 
   select(Freq) %>% 
   table(n_interaction = .) %>% 
@@ -14,7 +16,13 @@ table(dat[["doi"]]) %>%
   mutate(n_interaction = as.numeric(as.character(n_interaction))) %>% 
   ggplot(aes(x = n_interaction, y = Freq)) +
   geom_col() +
-  geom_text(x = 25, y = 40, label = paste0("Total number of publications: ", length(unique(dat[["doi"]])))) +
-  scale_x_continuous("Number of interactions per publication") +
-  scale_y_continuous("Number of publications") +
   theme_bw()
+
+if(!about_section)
+  p <- p + geom_text(x = 25, y = 40, label = paste0("Total number of publications: ", length(unique(dat[["doi"]])))) +
+  scale_x_continuous("Number of interactions per publication") +
+  scale_y_continuous("Number of publications")
+
+png(filename = "inst/AmyloGraph/manuals/database_summary.png", width = 960, height = 740)
+p
+dev.off()
