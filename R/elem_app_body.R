4b953e8d4c59cbe8c7232729b5dd9e2d4bd272f7
Laura <erdaradun.gaztea@gmail.com>
2021-09-29 20:22:43 +0200

    use AG option for side panel width

--- R/elem_app_body.R
+++ R/elem_app_body.R
@@ -1,5 +1,5 @@
 elem_app_body <- \(data_nodes, data_groups) fillRow(
   elem_filter_control_panel(data_groups),
   elem_app_main_panel(data_nodes),
-  flex = c(2, 10)
+  flex = c(ag_option("side_panel_width"), ag_option("main_panel_width"))
 )
