f7dee270276ca0839730f69f1125f4722ef7ce65
DominikRafacz <d.rafacz@student.mini.pw.edu.pl>
2021-09-30 12:01:43 +0200

    replace details button with AGID button, make it clickable
    solve #41

--- R/AGID_button.R
+++ R/AGID_button.R
@@ -1,11 +1,11 @@
 #' @importFrom purrr map_chr
 #' @importFrom glue glue
 #' @importFrom shiny actionButton
-details_button <- \(interaction_ids, id, ns, ...) {
+AGID_button <- \(interaction_ids, id, ns, ...) {
   map_chr(interaction_ids, 
           \(interaction_id) as.character(
             #should this values of id be generated manually with NS like this?
             actionButton(glue("ns(interaction_view_selector_{interaction_id})"), 
-                         "Details",
+                         as.character(interaction_id),
                          onclick = glue("Shiny.setInputValue('{NS('single_interaction', 'selected_interaction')}', '{interaction_id}')"))))
 }
--- R/render_interactions_subtable.R
+++ R/render_interactions_subtable.R
@@ -6,8 +6,8 @@ render_interactions_subtable <- function(input, edge_data, target_id, target_var
       filter({{target_id}} == input[["select_node"]]) %>%
       arrange({{target_variable}}, doi) %>%
       mutate(doi = linkify_doi(doi),
-             details = details_button(AGID)) %>%
-      select(AGID, details, {{target_variable}}, doi)
+             AGID = AGID_button(AGID)) %>%
+      select(AGID, {{target_variable}}, doi)
   )
   renderDataTable(
     rendered_data(),
