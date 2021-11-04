71e86fddfcef302f0e6ae2fa1a8bfbecb7f0b9fd
Michał Burdukiewicz <michalburdukiewicz@gmail.com>
2021-11-04 13:41:56 +0100

    added google analytics

--- R/render_ag_version.R
+++ R/render_ag_version.R
@@ -1,6 +1,20 @@
-#' @importFrom utils packageVersion
-render_ag_version <- function() {
-  renderText({
-    paste0("AmyloGraph version: ", packageVersion("AmyloGraph"), ".\n")
-  })
+render_ga <- function() {
+  tags$head(HTML(
+    "<script>
+      (function(i,s,o,g,r,a,m){
+        i['GoogleAnalyticsObject']=r;i[r]=i[r]||
+        function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();
+          a=s.createElement(o), m=s.getElementsByTagName(o)[0];
+          a.async=1;
+          a.src=g;m.parentNode.insertBefore(a,m)
+        })
+      (window, document, 'script',
+        '//www.google-analytics.com/analytics.js','ga');
+      
+        ga('create', 'G-0WVLM2F4MJ', 'auto');
+        ga('send', 'pageview');
+      
+      </script>"
+  ))
 }
--- R/ui.R
+++ R/ui.R
@@ -4,6 +4,7 @@
 #' @importFrom visNetwork visNetworkOutput
 ag_ui <- function(ag_data) fillPage(
   useShinyjs(),
+  render_ga(),
   elem_app_body(ag_data[["nodes"]], ag_data[["groups"]]),
   theme = "amylograph.css",
   title = "AmyloGraph"
