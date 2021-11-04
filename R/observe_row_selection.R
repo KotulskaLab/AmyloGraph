eda2222b4cadd9bc1eb71463ddb05e2e60b1307a
DominikRafacz <d.rafacz@student.mini.pw.edu.pl>
2021-11-04 16:37:35 +0100

    inactivating buttons

--- R/observe_row_selection.R
+++ R/observe_row_selection.R
@@ -1,11 +1,23 @@
-observe_row_selection <- function(ns, any_row_selected) observe({
-  toggleState(
-    selector = glue("#{ns('table')} .ag-download-button"),
-    condition = any_row_selected()
-  )
-  toggleCssClass(
-    class = "ag-download-button-disabled",
-    selector = glue("#{ns('table')} .ag-download-button"),
-    condition = !any_row_selected()
-  )
-})
+observe_row_selection <- function(ns, any_row_selected) {
+  observe({
+    toggleState(
+      selector = glue("#{ns('table')} .ag-download-button"),
+      condition = any_row_selected()
+    )
+    toggleCssClass(
+      class = "ag-download-button-disabled",
+      selector = glue("#{ns('table')} .ag-download-button"),
+      condition = !any_row_selected()
+    )
+    
+    toggleState(
+      selector = glue("#{ns('deselect_all')}"),
+      condition = any_row_selected()
+    )
+    toggleCssClass(
+      class = "ag-selection-button-disabled",
+      selector = glue("#{ns('deselect_all')}"),
+      condition = !any_row_selected()
+    )
+  })
+}
