f8f38aa118857e9e6d0ac2dac813069e82095103
Michał Burdukiewicz <michalburdukiewicz@gmail.com>
2021-11-04 13:54:38 +0100

    updated ga

--- R/render_ag_version.R
+++ R/render_ag_version.R
@@ -1,19 +1,39 @@
+# adapted from: https://github.com/DataScienceScotland/shiny_cookies
+
 render_ga <- function() {
   tags$head(HTML(
     "<script>
-      (function(i,s,o,g,r,a,m){
-        i['GoogleAnalyticsObject']=r;i[r]=i[r]||
-        function(){
-          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();
-          a=s.createElement(o), m=s.getElementsByTagName(o)[0];
-          a.async=1;
-          a.src=g;m.parentNode.insertBefore(a,m)
-        })
-      (window, document, 'script',
-        '//www.google-analytics.com/analytics.js','ga');
-      
-        ga('create', 'G-0WVLM2F4MJ', 'auto');
-        ga('send', 'pageview');
+var config = {
+    apiKey: '77d76ea90a9efadff2fb240e10e5af8680a529ae',
+    product: 'COMMUNITY',
+    text : {
+        thirdPartyTitle : 'Warning: Some cookies require your attention',
+        thirdPartyDescription : 'Consent for some third party cookies can not be automatically revoked. Please follow the link below if you want to opt out of them.'
+    },
+    optionalCookies: [
+        {
+            name : 'analytics',
+            label: 'Analytical Cookies',
+            description: 'Analytical cookies help us to improve our website by collecting and reporting information on its usage.',
+            cookies: ['_ga', '_gid', '_gat', '__utma', '__utmt', '__utmb', '__utmc', '__utmz', '__utmv'],
+            onAccept : function(){
+                // Add Google Analytics
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-0WVLM2F4MJ', { 'anonymize_ip':true });
+                // End Google Analytics
+            },
+            onRevoke: function(){
+                // Disable Google Analytics
+                window['ga-disable-G-0WVLM2F4MJ'] = true;
+                // End Google Analytics
+            }
+        }
+    ]
+};
+
+CookieControl.load( config );
       
       </script>"
   ))
