f6412e4b06b37fa18105cd4324c589adf6727231
Laura <erdaradun.gaztea@gmail.com>
2021-12-07 00:45:42 +0100

    make observe_selecting_all more readable

--- R/observe_selecting_all.R
+++ R/observe_selecting_all.R
@@ -7,7 +7,7 @@ observe_deselecting_all <- \(input, button_id, table_proxy) observeEvent(
 
 #' @importFrom DT selectRows
 #' @importFrom shiny observeEvent
-observe_selecting_all <- \(input, button_id, table_proxy, interactions_table) observeEvent(
+observe_selecting_all <- \(input, button_id, table_proxy, table_data) observeEvent(
   input[[button_id]],
-  selectRows(table_proxy, 1:nrow(interactions_table()))
+  selectRows(table_proxy, table_data() |> nrow() |> seq_len())
 )
