d21ea99eb3ef2e571c5bbadb4d20b0e7e4711ab0
Laura <erdaradun.gaztea@gmail.com>
2021-12-07 11:39:05 +0100

    group two highly related functions

--- R/reactive_selected_node.R
+++ R/reactive_selected_node.R
@@ -1,3 +1,7 @@
+reactive_selected_node_label <- \(selected_node_info) reactive({
+  selected_node_info()[["label"]]
+})
+
 #' @importFrom shiny reactive req
 #' @importFrom dplyr `%>%` filter
 reactive_selected_node_info <- \(input, node_data) reactive({
--- R/reactive_selected_node_label.R
+++
@@ -1,3 +0,0 @@
-reactive_selected_node_label <- \(selected_node_info) reactive({
-  selected_node_info()[["label"]]
-})
