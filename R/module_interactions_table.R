1f8c2ec25388d2cc65befb6ff939c32ee48a6b91
Laura <erdaradun.gaztea@gmail.com>
2021-12-23 22:04:50 +0100

    fix typo

--- R/module_interactions_table.R
+++ R/module_interactions_table.R
@@ -35,7 +35,7 @@ server_interactions_table <- function(id, edges, rvals) {
     observe_selecting_all(input, "select_all", table_proxy, interactions_table)
     
     output[["download_csv"]] <- table_download_handler(input, edges, write_csv, "csv")
-    output[["download_xlsx"]] <- table_download_handler(input, edges, write_xslx, "xlsx")
+    output[["download_xlsx"]] <- table_download_handler(input, edges, write_xlsx, "xlsx")
 
     # must be executed after assignment to the corresponding output
     outputOptions(output, "download_csv", suspendWhenHidden = FALSE)
