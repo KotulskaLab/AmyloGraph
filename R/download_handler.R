9d47c5d0f1b3a29377e30f70b455fd6e0427c239
Laura <erdaradun.gaztea@gmail.com>
2021-12-23 22:00:53 +0100

    implement network download as XGMML

--- R/download_handler.R
+++ R/download_handler.R
@@ -1,3 +1,4 @@
+#' @importFrom glue glue
 table_download_handler <- \(input, edges, write_function, extension)  downloadHandler(
   filename = \() glue("AmyloGraph.{extension}"),
   content = \(file) write_function(
@@ -7,3 +8,21 @@ table_download_handler <- \(input, edges, write_function, extension)  downloadHa
     file
   )
 )
+
+#' @importFrom BioNet saveNetwork
+XGMML_download_handler <- \(edges) {
+  downloadHandler(
+    filename = \() "AmyloGraph.XGMML",
+    content = \(file) saveNetwork(
+      igraph::graph_from_data_frame(
+        edges[["table"]],
+        # Will create igraph object will all nodes, even those with all interactions
+        # filtered out
+        vertices = ag_data_nodes()
+      ),
+      name = "AmyloGraph",
+      file = file,
+      type = "XGMML"
+    )
+  )
+}
