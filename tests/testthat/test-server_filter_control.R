45ef983f493827c65348fd0e791132f0cb28993d
Laura <erdaradun.gaztea@gmail.com>
2021-12-29 00:30:02 +0100

    reimplement graph control tests

---
+++ tests/testthat/test-server_filter_control.R
@@ -0,0 +1,39 @@
+server_args <- list(
+  data_interactions = ag_data[["interactions"]], data_groups = ag_data[["groups"]]
+)
+
+test_that("server_filter_control returns a reactiveValues object with 3 elements", {
+  testServer(server_filter_control, args = server_args, {
+    edges <- session$getReturned()
+    
+    expect_s3_class(edges, "reactivevalues")
+    expect_length(edges, 3)
+  })
+})
+
+test_that("filtering by motif doesn't increase the number of observations", {
+  testServer(server_filter_control, args = server_args, {
+    # Works when no motif set
+    session$setInputs(motif = "")
+    expect_gte(
+      nrow(interactions_filtered_by_group()),
+      nrow(interactions_filtered_by_motif())
+    )
+    # Works when motif set
+    session$setInputs(motif = "AG")
+    expect_gte(
+      nrow(interactions_filtered_by_group()),
+      nrow(interactions_filtered_by_motif())
+    )
+  })
+})
+
+test_that("incorrect motif is ignored", {
+  testServer(server_filter_control, args = server_args, {
+    session$setInputs(motif = "OBIVAD^^")
+    expect_equal(
+      nrow(interactions_filtered_by_group()),
+      nrow(interactions_filtered_by_motif())
+    )
+  })
+})
