71865a5a0291afdf0b08d3aebfa1ca6436c9467f
Laura <erdaradun.gaztea@gmail.com>
2021-12-28 23:56:32 +0100

    move some test variables to setup file

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,5 @@
 library(testthat)
 library(AmyloGraph)
+library(shiny, quietly = TRUE)
 
 test_check("AmyloGraph")
---
+++ tests/testthat/setup-ag_values.R
@@ -0,0 +1,12 @@
+ag_data <- ag_load_data()
+
+rvals <- reactiveValues(
+  table_visited = FALSE,
+  initally_selected = NULL
+)
+
+edges <- reactiveValues(
+  table = NULL,
+  graph = NULL,
+  node_info = NULL
+)
