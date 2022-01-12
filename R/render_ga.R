e918f137ac7503545738e360698bad24f57b7691
Michał Burdukiewicz <michalburdukiewicz@gmail.com>
2022-01-12 10:48:35 +0100

    added GA

--- R/render_ga.R
+++ R/render_ga.R
@@ -1,8 +1,5 @@
 # adapted from: https://github.com/DataScienceScotland/shiny_cookies
 
 render_ga <- function() {
-  tagList(HTML("<script src='https://cc.cdn.civiccomputing.com/8/cookieControl-8.x.min.js'></script>"),
-          HTML("<script async src='https://www.googletagmanager.com/gtag/js?id=G-0WVLM2F4MJ'></script>"),
-          tags$script(src = "cookie_control_config.js")
-  )
+  tags$head(includeHTML("./www/ga.html"))
 }
