8c79ad346cabecc01042f2bd9f67974983546e8a
Michał Burdukiewicz <michalburdukiewicz@gmail.com>
2022-01-13 15:22:44 +0100

    added new GA with GTM

--- R/render_ga.R
+++ R/render_ga.R
@@ -1,5 +1,6 @@
 # adapted from: https://github.com/DataScienceScotland/shiny_cookies
 
 render_ga <- function() {
-  tags$head(includeHTML("./www/ga.html"))
+  tagList(tags$head(includeHTML("./www/head-ga.html")),
+          tags$body(includeHTML("./www/body-ga.html")))
 }
