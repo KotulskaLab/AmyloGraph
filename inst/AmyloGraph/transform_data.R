d9ee501622e31dd31ba39171c91d1ae1b43658d1
Michał Burdukiewicz <michalburdukiewicz@gmail.com>
2022-01-28 21:00:50 +0100

    added references

--- inst/AmyloGraph/transform_data.R
+++ inst/AmyloGraph/transform_data.R
@@ -66,27 +66,26 @@ tmp <- readRDS("inst/AmyloGraph/AmyloGraph_proteins.RDS") %>%
 read.csv("inst/AmyloGraph/interactions_data.csv")[["doi"]]
 
 
-all_dois <- unique(c(read.csv("inst/AmyloGraph/interactions_data.csv")[["interactor_name"]], read.csv("inst/AmyloGraph/interactions_data.csv")[["interactee_name"]]))
-
-tmp <- read.csv("inst/AmyloGraph/interactions_data.csv")
-
-filter(tmp, interactee_name == "AIMP2")
+all_dois <- unique(read.csv("inst/AmyloGraph/interactions_data.csv")[["doi"]])
 
 doi_df <- cr_works(all_dois)
 
 nms <- lapply(doi_df[["data"]][["author"]], function(x) filter(x, sequence == "first"))
 
 all_names <- sapply(doi_df[["data"]][["author"]], function(x) {
-  paste0(apply(x, 1, function(ith_row) paste0(ith_row[1], " ", ith_row[2])), collapse = ", ")
+  paste0(apply(x, 1, function(ith_row) paste0(ith_row["given"], " ", ith_row["family"])), collapse = ", ")
 })
 #sapply(nms, function(i) paste0(substr(i[["given"]], 0, 1), " ", i[["family"]], " et al."))) %>% 
 
-bib_table <- data.frame(doi = doi_df[["data"]][["doi"]],
-                        nm = sapply(nms, function(i) i[["family"]]),
-                        all_names = all_names, 
-                        title = doi_df[["data"]][["container.title"]],
-                        year = doi_df[["data"]][["deposited"]]) %>% 
-  mutate(year = sapply(strsplit(year, "-"), function(i) i[[1]]))
+data.frame(doi = doi_df[["data"]][["doi"]],
+           nm = sapply(nms, function(i) i[["family"]]),
+           all_names = all_names, 
+           title = doi_df[["data"]][["title"]],
+           journal = doi_df[["data"]][["container.title"]],
+           year = doi_df[["data"]][["deposited"]]) %>% 
+  mutate(year = as.numeric(sapply(strsplit(year, "-"), function(i) i[[1]]))) %>%
+  write.csv("inst/AmyloGraph/reference_table.csv",
+            row.names = FALSE, fileEncoding = "UTF-8")
 
 
 
