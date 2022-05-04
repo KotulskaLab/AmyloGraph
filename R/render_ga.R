8fadcab9ff0e04dc3a637565b4692475f539406b
Laura <erdaradun.gaztea+github@gmail.com>
2022-05-05 00:27:31 +0200

    add importFrom

--- R/render_ga.R
+++ R/render_ga.R
@@ -1,5 +1,6 @@
 # adapted from: https://github.com/DataScienceScotland/shiny_cookies
 
+#' @importFrom shiny tagList tags includeHTML
 render_ga <- function() {
   tagList(tags$head(includeHTML("./www/head-ga.html")),
           tags$body(includeHTML("./www/body-ga.html")))
