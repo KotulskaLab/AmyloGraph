7738db543f61184cd11b6922cb7dea1812e795db
Laura <erdaradun.gaztea+github@gmail.com>
2022-05-05 00:45:24 +0200

    move comment to docs

--- R/render_ga.R
+++ R/render_ga.R
@@ -1,9 +1,8 @@
-# adapted from: https://github.com/DataScienceScotland/shiny_cookies
-
 #' @title Add Google Analytics to shiny app
 #' 
 #' @description Includes `<head>` and `<body>` tags with Google Analytics
-#' cookies scripts.
+#' cookies scripts. Adapted from:
+#' https://github.com/DataScienceScotland/shiny_cookies.
 #' 
 #' @return A list of tags to place inside a shiny app.
 #' 
