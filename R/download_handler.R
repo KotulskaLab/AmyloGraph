d24549eb4c1529d06cefecef5a966ab76f001ab8
Laura <erdaradun.gaztea+github@gmail.com>
2022-05-05 01:05:57 +0200

    document table_download_handler()

--- R/download_handler.R
+++ R/download_handler.R
@@ -1,15 +1,33 @@
+#' @title Handle download of AmyloGraph data
+#' 
+#' @description Creates a download handler that can be inserted into an app and
+#' used to download AmyloGraph data in various formats.
+#' 
+#' @param input \[\code{reactivevalues()}\]\cr
+#'  An input object of the app or a module that contains the table.
+#' @param edges \[\code{reactivevalues()}\]\cr
+#'  AmyloGraph data with "table" element.
+#' @param write_function \[\code{function(1)}\]\cr
+#'  A function that transforms table data to a file.
+#' @param extension \[\code{character(1)}\]\cr
+#'  File extension without a dot to append to filename (e.g. `"csv"`).
+#' 
+#' @return A `downloadHandler` object.
+#' 
 #' @importFrom shiny downloadHandler
 #' @importFrom dplyr `%>%` slice select
 #' @importFrom glue glue
-table_download_handler <- \(input, edges, write_function, extension)  downloadHandler(
-  filename = \() glue("AmyloGraph.{extension}"),
-  content = \(file) write_function(
-    edges[["table"]] %>%
-      slice(input[["table_rows_selected"]]) %>%
-      select(-c(from_id, to_id)),
-    file
+table_download_handler <- \(input, edges, write_function, extension)  {
+  downloadHandler(
+    filename = \() glue("AmyloGraph.{extension}"),
+    content = \(file) write_function(
+      edges[["table"]] %>%
+        slice(input[["table_rows_selected"]]) %>%
+        select(-c(from_id, to_id)),
+      file
+    )
   )
-)
+}
 
 #' @importFrom shiny downloadHandler
 #' @importFrom BioNet saveNetwork
