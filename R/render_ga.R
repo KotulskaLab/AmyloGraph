c207f314a74015c33bfb5d915c79ad36110a9da9
Laura <erdaradun.gaztea+github@gmail.com>
2022-05-05 00:44:40 +0200

    document render_ga()

--- R/render_ga.R
+++ R/render_ga.R
@@ -1,5 +1,12 @@
 # adapted from: https://github.com/DataScienceScotland/shiny_cookies
 
+#' @title Add Google Analytics to shiny app
+#' 
+#' @description Includes `<head>` and `<body>` tags with Google Analytics
+#' cookies scripts.
+#' 
+#' @return A list of tags to place inside a shiny app.
+#' 
 #' @importFrom shiny tagList tags includeHTML
 render_ga <- function() {
   tagList(tags$head(includeHTML("./www/head-ga.html")),
