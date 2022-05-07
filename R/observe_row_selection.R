887fd48f67993bba14cda0564d89672850e32274
Laura <erdaradun.gaztea+github@gmail.com>
2022-05-07 19:42:53 +0200

    document observe_deselect_button()

--- R/observe_row_selection.R
+++ R/observe_row_selection.R
@@ -41,6 +41,19 @@ observe_download_button <- function(ns, any_row_selected) toggle_state_and_css(
   any_row_selected
 )
 
+#' Observe "Deselect" button
+#' 
+#' @description Toggles button on and off based on whether any row is selected
+#' in a table.
+#' 
+#' @param ns \[\code{function(1)}\]\cr
+#'  Namespace-generating function that takes single string as the only argument.
+#' @param button_id \[\code{character(1)}\]\cr
+#'  ID of the "Deselect" button to observe.
+#' @param any_row_selected \[\code{reactive(logical(1))}\]\cr
+#'  A reactive value telling whether any row is selected in a table.
+#' 
+#' @importFrom glue glue
 observe_deselect_button <- function(ns, button_id, any_row_selected) toggle_state_and_css(
   glue("#{ns(button_id)}"),
   "ag-deselection-button-disabled",
