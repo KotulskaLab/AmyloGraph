05f7b6c2b93a0d32f7ee28c3257d5e28adee2c0e
Laura <erdaradun.gaztea+github@gmail.com>
2022-05-07 10:32:27 +0200

    document visResetEdges()

--- R/vis.R
+++ R/vis.R
@@ -1,3 +1,19 @@
+#' Clear and update edges in a graph
+#' 
+#' @description Clears all existing edges and creates new ones, then reselects
+#' a node.
+#' 
+#' @param graph \[\code{visNetwork_Proxy(1)}\]\cr
+#'  Proxy of a graph to update.
+#' @param edges \[\code{data.frame()}\]\cr
+#'  AmyloGraph graph data.
+#' @param input \[\code{reactivevalues()}\]\cr
+#'  An input object of the app or a module that contains the graph.
+#' @param selected_node_input_id \[\code{character(1)}\]\cr
+#'  ID of the selected node (protein) to reselect.
+#' 
+#' @return The same proxy as in `graph` parameter, but updated.
+#' 
 #' @importFrom visNetwork visGetEdges visRemoveEdges visUpdateEdges
 #' @importFrom dplyr `%>%`
 #' @importFrom purrr map_int
