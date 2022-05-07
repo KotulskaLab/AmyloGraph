10f540a2aad8f6c289d1923347245e0629bfd0a2
Laura <erdaradun.gaztea+github@gmail.com>
2022-05-07 19:28:58 +0200

    document toggle_state_and_css()

--- R/observe_row_selection.R
+++ R/observe_row_selection.R
@@ -1,3 +1,17 @@
+#' Enable/disable element with CSS styling
+#' 
+#' @description Toggles element state and sets CSS class to allow customizing
+#' the look of elements when disabled (very useful to differentiate them from
+#' enabled elements).
+#' 
+#' @param selector \[\code{character(1)}\]\cr
+#'  Query selector of the elements to target.
+#' @param class \[\code{character(1)}\]\cr
+#'  Class to add when condition is not satisfied.
+#' @param condition \[\code{function(1)}\]\cr
+#'  Function that takes no parameters and evaluates to either `TRUE` or `FALSE`.
+#' 
+#' @importFrom shinyjs toggleState toggleCssClass
 toggle_state_and_css <- function(selector, class, condition) observe({
   toggleState(
     selector = selector,
