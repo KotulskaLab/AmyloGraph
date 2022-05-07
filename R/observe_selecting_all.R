7fb1ca7ba646200049bd9539be11972c1dbd24f5
Laura <erdaradun.gaztea+github@gmail.com>
2022-05-07 19:08:29 +0200

    document update_deselecting_all()

--- R/observe_selecting_all.R
+++ R/observe_selecting_all.R
@@ -1,3 +1,15 @@
+#' Remove selections in a table
+#' 
+#' @description Sets selected rows to none in a table in response to clicking
+#' a button.
+#' 
+#' @param input \[\code{reactivevalues()}\]\cr
+#'  An input object of the app or a module that contains the table.
+#' @param button_id \[\code{character(1)}\]\cr
+#'  ID of a button that triggers removing selections.
+#' @param table_proxy \[\code{dataTableProxy(1)}\]\cr
+#'  Proxy of a table to update.
+#' 
 #' @importFrom DT selectRows
 #' @importFrom shiny observeEvent
 observe_deselecting_all <- \(input, button_id, table_proxy) observeEvent(
