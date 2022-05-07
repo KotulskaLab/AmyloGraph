f4e8fdb9e04a690e9046f29074a2f51272e93875
Laura <erdaradun.gaztea+github@gmail.com>
2022-05-07 19:18:17 +0200

    clear code

--- R/observe_selecting_all.R
+++ R/observe_selecting_all.R
@@ -12,7 +12,7 @@
 #' 
 #' @importFrom DT selectRows
 #' @importFrom shiny observeEvent
-observe_deselecting_all <- \(input, button_id, table_proxy) observeEvent(
+observe_deselecting_all <- function(input, button_id, table_proxy) observeEvent(
   input[[button_id]],
   selectRows(table_proxy, numeric())
 )
@@ -33,7 +33,7 @@ observe_deselecting_all <- \(input, button_id, table_proxy) observeEvent(
 #' 
 #' @importFrom DT selectRows
 #' @importFrom shiny observeEvent
-observe_selecting_all <- \(input, button_id, table_proxy, interactions_table) observeEvent(
+observe_selecting_all <- function(input, button_id, table_proxy, interactions_table) observeEvent(
   input[[button_id]],
   selectRows(table_proxy, 1:nrow(interactions_table()))
 )
