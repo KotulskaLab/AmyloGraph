3a9934a0c9bf05bea0b365c9def463f98824e982
Laura <erdaradun.gaztea+github@gmail.com>
2022-06-28 01:00:03 +0200

    document reactive_subtable_data()

--- R/reactive_table_data.R
+++ R/reactive_table_data.R
@@ -10,7 +10,7 @@
 #'  Namespace function to use for AGID buttons.
 #' 
 #' @return A \code{reactive} object with a \code{tibble} containing filtered
-#' interaction data. 
+#' interaction data.
 #' 
 #' @importFrom shiny reactive
 #' @importFrom dplyr mutate select `%>%`
@@ -33,6 +33,27 @@ reactive_table_data <- function(edges, ns) {
   })
 }
 
+#' Build table data for single protein
+#' 
+#' @description Mutates filtered interaction data for a single selected protein
+#' to contain DOI links and clickable AGIDs; then selects only columns that are
+#' displayed in the interactors or interactees table.
+#' 
+#' @param edges \[\code{reactivevalues()}\]\cr
+#'  AmyloGraph data with "table" element.
+#' @param ns \[code{function()}\]\cr
+#'  Namespace function to use for AGID buttons.
+#' @param input \[\code{reactivevalues()}\]\cr
+#'  An input object of the app or a module that contains the protein selector.
+#' @param target_id \[\code{NSE(1)}\]\cr
+#'  ID column name to use.
+#' @param target_variable \[\code{NSE(1)}\]\cr
+#'  Protein name column to use.
+#' 
+#' @return A \code{reactive} object with a \code{tibble} containing filtered
+#' interaction data for a single protein, either with interactors or
+#' interactees (depends on selected target ID).
+#' 
 #' @importFrom shiny reactive
 #' @importFrom dplyr filter arrange mutate select `%>%`
 reactive_subtable_data <- function(edges, ns, input, target_id, target_variable) {
