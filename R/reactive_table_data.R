d7e02e666056042c6b79cf4e9824cb5b144d996c
Laura <erdaradun.gaztea+github@gmail.com>
2022-06-28 00:38:50 +0200

    rename to reactive_table_data()

---
+++ R/reactive_table_data.R
@@ -0,0 +1,20 @@
+#' @importFrom shiny reactive
+#' @importFrom dplyr mutate select `%>%`
+#' @importFrom icecream ic
+reactive_table_data <- function(edges, ns) {
+  reactive({
+    ic(edges[["table"]]) %>% 
+      mutate(doi = linkify_doi(doi),
+             original_AGID = AGID,
+             AGID = AGID_button(AGID, ns)) %>% 
+      select(AGID,
+             interactor_name,
+             interactee_name,
+             aggregation_speed,
+             elongates_by_attaching,
+             heterogenous_fibers,
+             doi,
+             original_AGID)
+    # original_AGID must be last for column invisibility to work correctly
+  })
+}
