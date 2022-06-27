0415337c46e00c401ba63b40310066dfbc516930
Laura <erdaradun.gaztea+github@gmail.com>
2022-06-28 00:26:43 +0200

    rename and document reactive_allow_selection_transfer()

--- R/reactive_selecting_in_main_applicable.R
+++
@@ -1,5 +0,0 @@
-reactive_selecting_in_main_applicable <- \(input) reactive({
-  (!is.null(input[["interactees_rows_selected"]]) || 
-     !is.null(input[["interactors_rows_selected"]])) &&
-    !input[["ignore_filters"]]
-})
