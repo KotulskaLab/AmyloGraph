5bd33eb6d40a7050363ec532b41616c924c6da4b
Laura <erdaradun.gaztea+github@gmail.com>
2022-06-28 10:59:22 +0200

    document define_button()

--- R/util-buttons.R
+++ R/util-buttons.R
@@ -1,3 +1,18 @@
+#' Define a button type
+#' 
+#' @description Defines a button type with its visible component, server logic,
+#' and tags.
+#' 
+#' @param ui \[\code{function()}\]\cr
+#'  Mostly HTML part of a button that's generated during initialization of a
+#'  button instance. Must contain \code{ns} parameter.
+#' @param server \[\code{function()}\]\cr
+#'  Server logic that handles button-related actions. Must contain \code{input}
+#'  and \code{output} parameters.
+#' @param tags \[\code{character()}\]\cr
+#'  Categories to apply to the button type (defined in \code{BUTTON_TAGS}).
+#' 
+#' @return An \code{ag_button} object.
 define_button <- function(ui, server, tags = NULL) {
   structure(
     list(ui = ui, server = server, tags = tags),
