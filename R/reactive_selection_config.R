2aa1f5ac8e3b5e4f0b84eb670bf59291de322cba
Laura <erdaradun.gaztea+github@gmail.com>
2022-06-28 16:01:46 +0200

    document reactive_selection_config()

--- R/reactive_selection_config.R
+++ R/reactive_selection_config.R
@@ -1,3 +1,16 @@
+#' Construct DT selection config
+#' 
+#' @description Constructs selection configuration for the main table according
+#' to DataTable specification. It's required due to table proxy not being
+#' available before rendering the table first.
+#' 
+#' @param rvals \[\code{reactivevalues()}\]\cr
+#'  Information about table having been visited and rows selected in single
+#'  protein tables.
+#' 
+#' @return Value to use as `selection` parameter in `renderDataTable()`, either
+#' a list or a string.
+#' 
 #' @importFrom shiny reactive
 reactive_selection_config <- function(rvals) {
   reactive({
