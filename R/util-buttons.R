65c3881c04fd34a792279f01a552d39d30cfecf0
Laura <erdaradun.gaztea+github@gmail.com>
2022-06-28 10:25:36 +0200

    document collect_tags()

--- R/util-buttons.R
+++ R/util-buttons.R
@@ -1,3 +1,12 @@
+#' Collect button tags
+#' 
+#' @description This functions collects all unique tags assigned to supplied
+#' buttons.
+#' 
+#' @param buttons \[\code{ag_button()}\]\cr
+#'  Buttons whose tags to collect.
+#' 
+#' @return A vector of tags (a `character` vector).
 collect_tags <- function(buttons) {
   lapply(buttons, `[[`, "tags") %>%
     unlist(recursive = TRUE) %>%
