4443b2350615240a4ed44ed7ce94835fa889be09
Laura <erdaradun.gaztea+github@gmail.com>
2022-06-30 23:32:34 +0200

    implement visAGNetwork()

--- R/vis.R
+++ R/vis.R
@@ -1,3 +1,21 @@
+#' @importFrom visNetwork visNetwork visEdges visNodes visIgraphLayout
+#'  visPhysics visOptions visInteraction
+visAGNetwork <- function(nodes, edges) {
+  visNetwork(nodes = nodes, edges = edges, width = 1600, height = 900) %>%
+    visEdges(arrows = "to",
+             width = 2,
+             color = "#3674AB") %>%
+    visNodes(color = "#F3C677",
+             font = list(color = "#0C0A3E")) %>%
+    visIgraphLayout(smooth = TRUE, physics = FALSE, randomSeed = 1338) %>%
+    visPhysics(maxVelocity = 50, minVelocity = 49, timestep = 0.3) %>%
+    visOptions(
+      highlightNearest = list(enabled = TRUE, degree = 1,
+                              labelOnly = FALSE, hover = TRUE,
+                              algorithm = "hierarchical")) %>%
+    visInteraction(zoomView = TRUE, navigationButtons = TRUE)
+}
+
 #' Clear and update edges in a graph
 #' 
 #' @description Clears all existing edges and creates new ones, then reselects
