8032c2158b5065a020fa01acb1be39e6e2b023c3
Laura <erdaradun.gaztea+github@gmail.com>
2022-06-30 23:28:09 +0200

    document reactive_node_positions()

--- R/reactive_node_positions.R
+++ R/reactive_node_positions.R
@@ -1,3 +1,16 @@
+#' Retrieve node positions in a network
+#' 
+#' @description Triggers update of `network_positions` input value, then reads
+#' this value and formats it as a `data.frame`.
+#' 
+#' @param input \[\code{reactivevalues()}\]\cr
+#'  An input object of the app or a module that contains the network.
+#' @param network_id \[\code{character(1)}\]\cr
+#'  ID of the network.
+#' 
+#' @return A \code{reactive} object with `NULL` if no positions available or
+#' a `tibble` with `x`, `y` and `id` columns.
+#' 
 #' @importFrom dplyr bind_rows mutate
 #' @importFrom visNetwork visNetworkProxy visGetPositions
 reactive_node_positions <- function(input, network_id) {
