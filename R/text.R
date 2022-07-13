299169e2374378e5901c94d46ddd4e66240835d0
Laura <erdaradun.gaztea+github@gmail.com>
2022-07-14 00:00:53 +0200

    replace purrr::when() with switch()

--- R/text.R
+++ R/text.R
@@ -7,17 +7,15 @@
 #'  An internal name of an interaction attribute.
 #' 
 #' @return A single string with a human-readable question.
-#' 
-#' @importFrom purrr when
 text_question_attribute <- function(attribute) {
-  attribute %>%
-    when(
-      . == "aggregation_speed" ~
-        "Is the interactor affecting interactee's aggregating speed?",
-      . == "elongates_by_attaching" ~
-        "If interactee is still forming fibrils after the interaction, do fibrils of interactee elongates by attaching to monomers/oligomers/fibrils of interactor?",
-      . == "heterogenous_fibers" ~
-        "Is interaction resulting in heterogeneous fibrils consisting of interactor and interactee molecules?",
-      ~ stop(glue("Unknown attribute '{attribute}', no question found."))
-    )
+  switch(
+    attribute,
+    "aggregation_speed" =
+      "Is the interactor affecting interactee's aggregating speed?",
+    "elongates_by_attaching" =
+      "If interactee is still forming fibrils after the interaction, do fibrils of interactee elongates by attaching to monomers/oligomers/fibrils of interactor?",
+    "heterogenous_fibers" =
+      "Is interaction resulting in heterogeneous fibrils consisting of interactor and interactee molecules?",
+    stop(glue("Unknown attribute '{attribute}', no question found."))
+  )
 }
