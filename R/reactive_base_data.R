c6701bc70d9de8e0521421ebf25652cf3de55d71
Laura <erdaradun.gaztea+github@gmail.com>
2022-07-15 11:58:57 +0200

    split reactive_base_data() into smaller functions

--- R/reactive_base_data.R
+++ R/reactive_base_data.R
@@ -1,20 +1,27 @@
-#' @importFrom dplyr filter
-#' @importFrom purrr map
-#' @importFrom rlang sym expr
 reactive_base_data <- function(input, motif) {
   reactive({
-    ret <- ag_data_interactions %>%
-      filter(!!!map(
-        unname(ag_data_group_labels),
-        ~ expr(!!sym(.) %in% !!input[[.]]))
-      )
-    
-    if (is_valid(motif()) && nchar(motif()) > 0) {
-      ret %>%
-        filter(contains_motif(interactor_sequence, motif()) |
-                 contains_motif(interactee_sequence, motif()))
-    } else {
-      ret
-    }
+    ag_data_interactions %>%
+      filter_by_attribute(input, "aggregation_speed") %>%
+      filter_by_attribute(input, "elongates_by_attaching") %>%
+      filter_by_attribute(input, "heterogenous_fibers") %>%
+      filter_by_motif(motif())
   })
+}
+
+#' @importFrom dplyr filter
+#' @importFrom rlang sym
+filter_by_attribute <- function(data, input, attribute_label) {
+  data %>%
+    filter(!!sym(attribute_label) %in% input[[attribute_label]])
+}
+
+#' @importFrom dplyr filter
+filter_by_motif <- function(data, motif) {
+  if (is_valid(motif) && nchar(motif) > 0) {
+    data %>%
+      filter(contains_motif(interactor_sequence, motif) |
+               contains_motif(interactee_sequence, motif))
+  } else {
+    data
+  }
 }
