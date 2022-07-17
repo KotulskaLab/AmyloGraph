6e3b4b412384999d1c388a3a3fe23ca2b517d3c1
Laura <erdaradun.gaztea+github@gmail.com>
2022-07-17 13:56:11 +0200

    use new module in filter control

--- R/module_filter_control.R
+++ R/module_filter_control.R
@@ -1,21 +1,12 @@
-#' @importFrom purrr imap
 ui_filter_control <- function(id) {
   ns <- NS(id)
   div(
     id = id,
     ui_group_edges(ns("group_edges")),
     ui_motif_filter(ns("motif")),
-    do.call(
-      tagList,
-      imap(
-        ag_data_group_labels,
-        ~ filterCheckboxInput(
-          ns(.x),
-          ag_data_attribute_values[[.x]],
-          .y
-        )
-      )
-    )
+    ui_attribute_filter(ns("aggregation_speed"), "aggregation_speed"),
+    ui_attribute_filter(ns("elongates_by_attaching"), "elongates_by_attaching"),
+    ui_attribute_filter(ns("heterogenous_fibers"), "heterogenous_fibers")
   )
 }
 
@@ -25,15 +16,22 @@ server_filter_control <- function(id) {
   moduleServer(id, function(input, output, session) {
     group <- server_group_edges("group_edges")
     motif <- server_motif_filter("motif")
+    aggregation_speed <- server_attribute_filter("aggregation_speed")
+    elongates_by_attaching <- server_attribute_filter("elongates_by_attaching")
+    heterogenous_fibers <- server_attribute_filter("heterogenous_fibers")
     
     # TODO: extract as observer
     observe({
       walk(ag_data_group_labels,
-           ~ toggleCssClass(.x, "filter_checkbox_active",
-                            group() == .x))
+           ~ toggleCssClass(
+             id = NS(.x, "filter"),
+             class = "filter_checkbox_active",
+             condition = group() == .x)
+           )
     })
     
-    base_data <- reactive_base_data(input, motif)
+    base_data <- reactive_base_data(aggregation_speed, elongates_by_attaching,
+                                    heterogenous_fibers, motif)
     graph_data <- reactive_graph_data(base_data, group)
     
     # TODO: return list of reactives instead and extract them using %<-% or sth
--- R/reactive_base_data.R
+++ R/reactive_base_data.R
@@ -1,18 +1,19 @@
-reactive_base_data <- function(input, motif) {
+reactive_base_data <- function(aggregation_speed, elongates_by_attaching,
+                               heterogenous_fibers, motif) {
   reactive({
     ag_data_interactions %>%
-      filter_by_attribute(input, "aggregation_speed") %>%
-      filter_by_attribute(input, "elongates_by_attaching") %>%
-      filter_by_attribute(input, "heterogenous_fibers") %>%
+      filter_by_attribute(aggregation_speed(), "aggregation_speed") %>%
+      filter_by_attribute(elongates_by_attaching(), "elongates_by_attaching") %>%
+      filter_by_attribute(heterogenous_fibers(), "heterogenous_fibers") %>%
       filter_by_motif(motif())
   })
 }
 
 #' @importFrom dplyr filter
 #' @importFrom rlang sym
-filter_by_attribute <- function(data, input, attribute_label) {
+filter_by_attribute <- function(data, values, label) {
   data %>%
-    filter(!!sym(attribute_label) %in% input[[attribute_label]])
+    filter(!!sym(label) %in% values)
 }
 
 #' @importFrom dplyr filter
